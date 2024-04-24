f3fb18dd0c5e453c83f7519d9775a5d1660dbb75
Katy <115113751+ShidliKaty@users.noreply.github.com>
2024-04-24 12:48:05 +0300

    create pages

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+export default function MealsDetailsPage() {
+    return (
+      <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+            MealsDetailsPage
+        </h1>
+      </main>
+    );
+  }
